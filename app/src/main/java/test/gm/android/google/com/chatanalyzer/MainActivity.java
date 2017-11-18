2ac01628f46a111c8f72ff66bb2f51a6d384987f
Daniel Bücheler <dbahn25@t-online.de>
2017-11-18 23:35:38 +0100

    finished

--- app/src/main/java/test/gm/android/google/com/chatanalyzer/MainActivity.java
+++ app/src/main/java/test/gm/android/google/com/chatanalyzer/MainActivity.java
@@ -6,17 +6,26 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Set;
 
@@ -26,6 +35,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     Button btn;
     ProgressBar pgb;
+
+    Uri fileUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +56,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String text = b.getString(Intent.EXTRA_TEXT);
             Log.d(TAG, String.valueOf(((ArrayList<Uri>) b.get(Intent.EXTRA_STREAM)).get(0)));
             Log.d(TAG, i.getExtras().keySet().toString());
-
-            ContentValues val = new ContentValues();
-            val.put("action", "parseChat");
-            val.put("debug", true);
-
-
+            fileUri = ((ArrayList<Uri>) b.get(Intent.EXTRA_STREAM)).get(0);
         }
 
 
@@ -61,23 +67,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         pgb.setVisibility(ProgressBar.VISIBLE);
 
         SendChatsAsync async = new SendChatsAsync();
-        async.execute("action", "parseChat");
 
-        Intent i = new Intent(this, WebViewActivity.class);
-        i.putExtra("URL", "http://example.com");
-        startActivity(i);
+        String fullChat = "";
+        StringBuffer buf = new StringBuffer();
+
+        // convert URI file to String
+
+        try {
+            InputStream is = getContentResolver().openInputStream(fileUri);
+            BufferedReader r = new BufferedReader(new InputStreamReader(is));
+            if(is != null) {
+                while((fullChat = r.readLine()) != null) {
+                    buf.append(fullChat+"\n");
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        fullChat = buf.toString();
+
+        //Log.d(TAG, fullChat);
+        //Log.d(TAG, URLEncoder.encode(fullChat));
+
+        Log.d(TAG, "async " + async.execute(fullChat));
     }
 
     private class SendChatsAsync extends AsyncTask<String, Void, String> {
 
+        String response = "";
+
         /**
          *
-         * @param input First value:
+         * @param input
          * @return
          */
         @Override
         protected String doInBackground(String... input) {
             try {
+
+
                 URL url = new URL("http://chat-analyzer-server.azurewebsites.net/api");
                 ContentValues val = new ContentValues();
 
@@ -86,7 +117,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 con.setDoOutput(true);
                 con.setDoInput(true);
 
-                String reqBody = input[0] + "=" + input[1] + "&debug=true";
+
+
+                String reqBody = "action=registerChat&chat=" + URLEncoder.encode(input[0]);
 
                 DataOutputStream s = new DataOutputStream(con.getOutputStream());
                 s.writeBytes(reqBody);
@@ -96,6 +129,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 con.connect();
 
                 int responseCode = con.getResponseCode();
+                if(responseCode >= 200 && responseCode < 300) { // 2XX: success
+                    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String inputLine;
+
+                    StringBuffer buf = new StringBuffer();
+
+                    while((inputLine = in.readLine()) != null) {
+                        buf.append(inputLine);
+                    }
+                    response = buf.toString();
+                    Log.d(TAG, response);
+                }
                 Log.d(TAG, "POST: " + responseCode);
 
             } catch (MalformedURLException e) {
@@ -105,7 +150,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
 
 
-            return null;
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Intent i = new Intent(getApplicationContext(), WebViewActivity.class);
+            i.putExtra("URL", response);
+            startActivity(i);
         }
     }
 }
